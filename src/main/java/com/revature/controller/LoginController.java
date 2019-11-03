4f53c8225e32b13aac04b0eac65481c61b08ec3d
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-11-03 15:12:07 -0500

    Added LoginController with debug tests.

--- src/main/java/com/revature/controller/LoginController.java
+++ src/main/java/com/revature/controller/LoginController.java
@@ -1,8 +1,29 @@
 package com.revature.controller;
 
+import static com.revature.util.LoggerUtil.log;
+
+import org.hibernate.SessionFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.util.SessionFactoryUtil;
+
 @RestController
+@RequestMapping("/login")
 public class LoginController {
+	private SessionFactory sf = SessionFactoryUtil.getSessionFactory();
 	
+	@GetMapping
+	public String loginGet() {
+		log.debug("Hello, get.");
+		return "Login Getto!";
+	}
+	
+	@PostMapping
+	public String loginPost() {
+		log.debug("Hello, post.");
+		return "Login Postto!";
+	}
 }
